6942aa5775e9eadda9fa7cbd87495b29561743d4
joycezhang <787027175@qq.com>
2022-02-21 11:58:21 +0800

    图形验证码、服务发现

---
+++ src/ServiceProvider.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace JoyceZ\LaravelLib;
+
+use Illuminate\Support\ServiceProvider as LaravelServiceProvider;
+use JoyceZ\LaravelLib\Contracts\Captcha as CaptchaContract;
+use JoyceZ\LaravelLib\Services\Captcha\Image\Captcha;
+use Laravel\Lumen\Application as LumenApplication;
+
+class ServiceProvider extends LaravelServiceProvider
+{
+
+    public function boot()
+    {
+
+    }
+
+    /**
+     * 设置配置文件
+     */
+    protected function setupConfig()
+    {
+        $source = realpath(__DIR__ . '/config.php');
+        if ($this->app instanceof LaravelApplication && $this->app->runningInConsole()) {
+            $this->publishes([$source => config_path('laraveladmin.php')], 'joyce-config');
+        } elseif ($this->app instanceof LumenApplication) {
+            $this->app->configure('laraveladmin');
+        }
+
+        $this->mergeConfigFrom($source, 'laraveladmin');
+    }
+
+    public function register()
+    {
+        $this->setupConfig();
+        /**
+         * 绑定图形验证码
+         */
+        $this->app->bind(CaptchaContract::class, function () {
+            $captcha = new Captcha();
+            $config = collect(config('laraveladmin.captcha'))->filter(function ($value) {
+                return !empty($value);
+            })->toArray();
+            $captcha->withConfig($config);
+            return $captcha;
+        });
+    }
+
+}
