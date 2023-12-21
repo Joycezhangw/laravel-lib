41337bf87fb468de362d61bf115d3788e6794df7
joycezhang <787027175@qq.com>
2023-12-21 15:57:39 +0800

    ✨ (BaseModel):  更新model 生成

---
+++ src/Model/BaseModel.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace JoyceZ\LaravelLib\Model;
+
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Carbon;
+
+class BaseModel extends Model
+{
+    /**
+     * 重写日期序列化
+     * @param DateTimeInterface $date
+     * @return string
+     */
+    protected function serializeDate(DateTimeInterface $date)
+    {
+        return $date->format(Carbon::parse($date)->toDateTimeString());
+    }
+}
