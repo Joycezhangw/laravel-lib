e62734c596e94f5ad6d43ce6f0063fc07e3e2f79
joycezhang <787027175@qq.com>
2024-03-11 00:15:15 +0800

    ✨ (Model): EncryptTableDb 字段值加解密

---
+++ src/Model/Casts/EncryptTableDb.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace JoyceZ\LaravelLib\Model\Casts;
+
+use Illuminate\Contracts\Database\Eloquent\CastsAttributes;
+use Illuminate\Database\Eloquent\Model;
+use JoyceZ\LaravelLib\Exceptions\DecryptErrorException;
+use JoyceZ\LaravelLib\Security\AopSecurity;
+
+/**
+ * 对需要加密的字段进行加解密处理
+ * 用于需要对称加解密，且需要条件查询字段
+ */
+class EncryptTableDb implements CastsAttributes
+{
+    /**
+     * 将不为空的值进行加密
+     * @param Model $model
+     * @param string $key
+     * @param mixed $value
+     * @param array $attributes
+     * @return mixed|string|null
+     */
+    public function set($model, string $key, $value, array $attributes)
+    {
+        //值为空不进行加密
+        if (!is_null($value) && $value !== '') {
+            return (new AopSecurity())->withScrectKey()->encrypt($value);
+        }
+        return $value;
+    }
+
+    /**
+     * 将取出的值进行解密
+     * @param Model $model
+     * @param string $key
+     * @param mixed $value
+     * @param array $attributes
+     * @return false|string|null
+     * @throws \Exception
+     */
+    public function get($model, string $key, $value, array $attributes)
+    {
+        try {
+            if (!is_null($value) && $value !== '') {
+                return (new AopSecurity())->withScrectKey()->decrypt($value);
+            }
+            return $value;
+        } catch (DecryptErrorException $exception) {
+            throw new \Exception($exception->getMessage());
+        }
+    }
+
+}
